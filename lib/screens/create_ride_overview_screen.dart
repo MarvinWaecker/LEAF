57c01ed3e449c815bffa5c07c19ca085e7741d27
Paul <paul.bloching@gmail.com>
2020-01-12 23:13:29 +0100

    almost done

---
+++ lib/screens/create_ride_overview_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CreateRideOverviewScreen extends StatefulWidget {
+  final String origin, destination, date, time;
+  CreateRideOverviewScreen(this.origin, this.destination, this.date, this.time);
+
+  @override
+  _CreateRideOverviewScreenState createState() =>
+      _CreateRideOverviewScreenState();
+}
+
+class _CreateRideOverviewScreenState extends State<CreateRideOverviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff111e2e),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xff111e2e),
+        automaticallyImplyLeading: true,
+        title: Text(
+          'Übersicht',
+          style: TextStyle(
+            fontFamily: 'UbuntuRegular',
+            fontSize: 24,
+            color: Color(0xffE6EFE9),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.navigate_before,
+            color: Color(0xffe8b641),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Text(
+          widget.time,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
