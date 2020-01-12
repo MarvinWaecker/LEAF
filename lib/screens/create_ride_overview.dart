fba41dcd9fd6efcc88c01e88315d3c5c139ef435
Paul <paul.bloching@gmail.com>
2020-01-12 18:01:16 +0100

    first step

---
+++ lib/screens/create_ride_overview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateRideOverview extends StatefulWidget {
+  @override
+  _CreateRideOverviewState createState() => _CreateRideOverviewState();
+}
+
+class _CreateRideOverviewState extends State<CreateRideOverview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
